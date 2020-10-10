5590e5f1ab2f0d9ebb1b7d0ad0ce3011af436f61
Tamukong Brian <briantbrian4@outlook.com>
2020-10-10 10:21:54 +0100

    First commit

---
+++ resources/views/vendor/shop/account/index.blade.php
@@ -0,0 +1,28 @@
+@extends('shop::base')
+
+@section('aimeos_header')
+    <?= $aiheader['locale/select'] ?>
+    <?= $aiheader['basket/mini'] ?>
+    <?= $aiheader['account/profile'] ?>
+    <?= $aiheader['account/history'] ?>
+    <?= $aiheader['account/favorite'] ?>
+    <?= $aiheader['account/watch'] ?>
+    <?= $aiheader['catalog/session'] ?>
+@stop
+JALJLKAJL
+
+@section('aimeos_head')
+    <?= $aibody['locale/select'] ?>
+    <?= $aibody['basket/mini'] ?>
+@stop
+
+@section('aimeos_body')
+    <?= $aibody['account/profile'] ?>
+    <?= $aibody['account/history'] ?>
+    <?= $aibody['account/favorite'] ?>
+    <?= $aibody['account/watch'] ?>
+@stop
+
+@section('aimeos_aside')
+    <?= $aibody['catalog/session'] ?>
+@stop
